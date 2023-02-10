28230020ca96fd0791676549226b4ecad665e0ee
fayesadev <fayedumbrigue@gmail.com>
2023-02-10 04:33:30 +0000

    Create INSERT query for addProperty

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -102,7 +102,7 @@ const getAllReservations = function(guest_id, limit = 10) {
       `, [guest_id, limit])
   .then((result) => {
     // console.log(getAllProperties(result.rows[0], limit = 10));
-    return getAllProperties(result.rows[0], limit = 10);
+    return getAllProperties(result.rows, limit = 10);
   })
   .catch((err) => {
     console.log(err.message);
@@ -133,12 +133,11 @@ const getAllProperties = function(options, limit = 10) {
     queryString += `WHERE city LIKE $${queryParams.length} `;
   }
 
-
   if (options.owner_id && queryParams.length === 0) {
-    queryParams.push(`%${options.owner_id}%`);
+    queryParams.push(options.owner_id);
     queryString += `WHERE owner_id = $${queryParams.length} `;
   } else if (options.owner_id) {
-    queryParams.push(`%${options.owner_id}%`);
+    queryParams.push(options.owner_id);
     queryString += `AND owner_id = $${queryParams.length} `;
   }
   
@@ -171,26 +170,13 @@ const getAllProperties = function(options, limit = 10) {
   LIMIT $${queryParams.length};
   `;
 
-  console.log("querystring", queryString, "queryparams", queryParams);
+  // console.log("querystring", queryString, "queryparams", queryParams);
 
-  return pool.query(queryString, queryParams).then((res) => res.rows);
-      // WHERE city LIKE '%ancouve%'
-      // GROUP BY properties.id
-      // HAVING avg(property_reviews.rating) >= 4
-      // ORDER BY cost_per_night
-      // LIMIT $2;
-
-  // return pool
-  //   .query(``, [options, limit])
-  //   .then((result) => {
-  //     // console.log(result.rows);
-  //     return result.rows;
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //   });
+  return pool.query(queryString, queryParams)
+    .then((res) => res.rows)
+    .catch((err) => {console.log(err.message)});
 }
-// `SELECT * FROM properties LIMIT $1`
+
 exports.getAllProperties = getAllProperties;
 
 
@@ -200,9 +186,16 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;`;
+
+  console.log("querystring", queryString, "queryParams", queryParams);
+  return pool.query(queryString, queryParams)
+    .then((res) => res.rows)
+    .catch((err) => {console.log(err.message)});
 }
 exports.addProperty = addProperty;
