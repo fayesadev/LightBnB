6501a37eb5b43d67ceca7ad2b82fb6a72df95ff7
fayesadev <fayedumbrigue@gmail.com>
2023-02-10 02:41:37 +0000

    Implement filter queries for getAllProperties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,6 +1,7 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 const { Pool } = require('pg');
+const { query } = require('express');
 
 const pool = new Pool({
   user: 'vagrant',
@@ -100,7 +101,7 @@ const getAllReservations = function(guest_id, limit = 10) {
       LIMIT $2;
       `, [guest_id, limit])
   .then((result) => {
-    console.log(getAllProperties(result.rows[0], limit = 10));
+    // console.log(getAllProperties(result.rows[0], limit = 10));
     return getAllProperties(result.rows[0], limit = 10);
   })
   .catch((err) => {
@@ -119,16 +120,77 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      // console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  const queryParams = [];
+  
+  let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON property_id = properties.id
+    `;
+  
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+
+  if (options.owner_id && queryParams.length === 0) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += `WHERE owner_id = $${queryParams.length} `;
+  } else if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+  
+  if (options.minimum_price_per_night && queryParams.length === 0) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `WHERE cost_per_night >= $${queryParams.length} `;
+  } else if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night && queryParams.length === 0) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `WHERE cost_per_night <= $${queryParams.length} `;
+  } else if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+  }
+
+  queryString += `GROUP BY properties.id `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log("querystring", queryString, "queryparams", queryParams);
+
+  return pool.query(queryString, queryParams).then((res) => res.rows);
+      // WHERE city LIKE '%ancouve%'
+      // GROUP BY properties.id
+      // HAVING avg(property_reviews.rating) >= 4
+      // ORDER BY cost_per_night
+      // LIMIT $2;
+
+  // return pool
+  //   .query(``, [options, limit])
+  //   .then((result) => {
+  //     // console.log(result.rows);
+  //     return result.rows;
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.message);
+  //   });
 }
+// `SELECT * FROM properties LIMIT $1`
 exports.getAllProperties = getAllProperties;
 
 
